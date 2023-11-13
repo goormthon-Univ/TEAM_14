eafe1bbb44312a768122cbf2948133f587285944
ooing-0720 <ooing-0720@naver.com>
2023-11-13 21:36:37 +0900

    BE_first commit

---
+++ src/main/java/com/mungge/ormi/OrmiApplication.java
@@ -0,0 +1,13 @@
+package com.mungge.ormi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mungge/ormi/OrmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mungge.ormi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
