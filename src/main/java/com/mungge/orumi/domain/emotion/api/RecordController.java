2f0d71d261abc6d99ab94d65b3441379c9c64031
ooing-0720 <ooing-0720@naver.com>
2023-11-16 23:29:25 +0900

    BE_refactor 감정 기록(날씨, 하늘) 리팩토링

--- src/main/java/com/mungge/orumi/domain/emotion/api/RecordController.java
+++ src/main/java/com/mungge/orumi/domain/emotion/api/RecordController.java
@@ -12,26 +12,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/record")
 public class RecordController {
 
-    private final EmotionCountService diaryEmotionService;
+    private final EmotionCountService emotionCountService;
+    private static String id = "orumi";
 
     @Operation(summary = "감정 날씨", tags = "Record Controller")
     @ApiResponse(responseCode = "200", description = "OK",
-                    content = @Content(schema = @Schema(implementation = WeatherDto.class)))
+            content = @Content(schema = @Schema(implementation = WeatherDto.class)))
     @ApiResponse(responseCode = "400", description = "BAD REQUEST")
     @ApiResponse(responseCode = "404", description = "NOT FOUND")
     @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
-    @GetMapping("/record/weather")
-    public ResponseEntity<?> getEmotionWeather(String id) {
-        List<Emotion> list = diaryEmotionService.getEmotionsOfMonth(id);
+    @GetMapping("/weather")
+    public ResponseEntity<?> getEmotionWeather() {
+
+        List<Emotion> list = emotionCountService.getEmotionsOfMonth(id);
         WeatherDto weatherDto = new WeatherDto(id, list);
         return ResponseEntity.ok(weatherDto);
     }
@@ -42,10 +45,19 @@ public class RecordController {
     @ApiResponse(responseCode = "400", description = "BAD REQUEST")
     @ApiResponse(responseCode = "404", description = "NOT FOUND")
     @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
-    @GetMapping("/record/sky")
-    public ResponseEntity<?> getEmotionSky(String id) {
-        List<Integer> list = diaryEmotionService.getAllCountByEmotion(id);
+    @GetMapping("/sky")
+    public ResponseEntity<?> getEmotionSky() {
+        List<Integer> list = emotionCountService.getAllCountByEmotion(id);
         SkyDto skyDto = new SkyDto(id, list.get(0), list.get(1), list.get(2), list.get(3), list.get(4), list.get(5));
         return ResponseEntity.ok(skyDto);
+    }
+
+    @Operation(summary = "감정 구름 횟수", tags = "Record Controller")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(schema = @Schema(implementation = Integer.class)))
+    @GetMapping("/emotion")
+    public ResponseEntity<?> getEmotionCount(@RequestParam String emotion) {
+        int count = emotionCountService.getCountOfEmotion(id, emotion);
+        return ResponseEntity.ok(count);
     }
 }
--- src/main/java/com/mungge/orumi/domain/emotion/dao/EmotionCountRepository.java
+++ src/main/java/com/mungge/orumi/domain/emotion/dao/EmotionCountRepository.java
@@ -1,26 +1,9 @@
 package com.mungge.orumi.domain.emotion.dao;
 
-import com.mungge.orumi.domain.diary.domain.Diary;
+import com.mungge.orumi.domain.emotion.domain.EmotionCount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+public interface EmotionCountRepository extends JpaRepository<EmotionCount, Long> {
 
-@Repository
-public interface EmotionCountRepository extends JpaRepository<Diary, Long> {
-    /* 감정 하늘 (감정별로 총 6번 쿼리 실행)
-     * SELECT COUNT(*)
-     * FROM dairy
-     * WHERE user_id = {userId} AND emotion = {emotion} AND date >= {start} AND date <= {today}
-     */
-    public int countAllByUserIdAndEmotionAndDateBetween(String userId, int emotion, LocalDate start, LocalDate today);
-
-    /*
-     * 감정 날씨 (오름차순으로 이번 달의 모든 감정 출력)
-     * SELECT emotion FROM Dairy
-     * WHERE user_id = {userId} AND date >= {start} AND date <= {today}
-     * ORDER BY date ASC
-     */
-    public List<Diary> findByUserIdAndDateBetween(String userId, LocalDate start, LocalDate today);
+    public EmotionCount findByUserId(String userId);
 }
